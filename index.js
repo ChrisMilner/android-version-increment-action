b1f8bc23eb3123aa41e86ebf53a137f3ef9cbb27
Chris Milner <christopherdmilner@gmail.com>
2025-10-22 22:05:44 +0100

    Generalise the solution to work with any valid prefix

--- index.js
+++ index.js
@@ -3,14 +3,8 @@ const core = require('@actions/core');
 
 const ALLOWED_INCREMENT_TYPES = ['major', 'minor', 'patch'];
 
-const VERSION_CODE_REGEX_BY_FORMAT = {
-    "groovy": /versionCode ([0-9]+)/,
-    "kotlin": /versionCode = ([0-9]+)/,
-};
-const VERSION_NAME_REGEX_BY_FORMAT = {
-    "groovy": /versionName "([0-9]+).([0-9]+).([0-9]+)"/,
-    "kotlin": /versionName = "([0-9]+).([0-9]+).([0-9]+)"/,
-};
+const VERSION_CODE_REGEX = /(versionCode\s*=?\s*)([0-9]+)/;
+const VERSION_NAME_REGEX =  /(versionName\s*=?\s*?)"([0-9]+).([0-9]+).([0-9]+)"/;
 
 const getNewVersionName = (major, minor, patch, incrementType) => {
     if (incrementType === 'major') {
@@ -28,8 +22,6 @@ const getNewVersionName = (major, minor, patch, incrementType) => {
 
 const formatVersion = (major, minor, patch) => `${major}.${minor}.${patch}`;
 
-const getFormatFromFileName = (fileName) => fileName.endsWith('.kts') ? 'kotlin' : 'groovy';
-
 try {
     const pathToBuildFile = core.getInput('build-gradle-path');
     const incrementType = core.getInput('name-increment-type').toLowerCase();
@@ -41,12 +33,8 @@ try {
 
     const buildFileContents = fs.readFileSync(pathToBuildFile, { encoding: 'utf8' });
 
-    const fileFormat = getFormatFromFileName(pathToBuildFile);
-
-    console.log(`Detected build file format: ${fileFormat}`);
-
-    const [, code] = VERSION_CODE_REGEX_BY_FORMAT[fileFormat].exec(buildFileContents);
-    const [, major, minor, patch] = VERSION_NAME_REGEX_BY_FORMAT[fileFormat].exec(buildFileContents);
+    const [, codePrefix, code] = VERSION_CODE_REGEX.exec(buildFileContents);
+    const [, versionPrefix, major, minor, patch] = VERSION_NAME_REGEX.exec(buildFileContents);
 
     const newCode = parseInt(code) + 1;
     const newName = getNewVersionName(major, minor, patch, incrementType);
@@ -54,11 +42,9 @@ try {
     console.log(`Version Code: ${code} => ${newCode}`);
     console.log(`Version Name: ${formatVersion(major, minor, patch)} => ${newName}`);
 
-    const keyValueSeparator = fileFormat == "kotlin" ? " = " : " ";
-
     const newFileContents = buildFileContents
-        .replace(`versionCode${keyValueSeparator}${code}`, `versionCode${keyValueSeparator}${newCode}`)
-        .replace(`versionName${keyValueSeparator}"${formatVersion(major, minor, patch)}"`, `versionName${keyValueSeparator}"${newName}"`);
+        .replace(`${codePrefix}${code}`, `${codePrefix}${newCode}`)
+        .replace(`${versionPrefix}"${formatVersion(major, minor, patch)}"`, `${versionPrefix}"${newName}"`);
 
     fs.writeFileSync(pathToBuildFile, newFileContents);
 
